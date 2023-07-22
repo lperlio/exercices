4f819a1cb5ee49e9e07efc0b21c0402f7bfb54a0
lperlio <lionel.person@gmail.com>
2023-07-22 16:17:35 +0200

    Cours_2_suite_2

--- Cours-P2/index.js
+++ Cours-P2/index.js
@@ -25,3 +25,95 @@ btn2.addEventListener('click', () => {
   response.classList.add('show-response');
   response.style.background = 'red';
 });
+
+//..................................................................
+
+// Mouse Events
+const mousemove = document.querySelector('.mousemove');
+
+window.addEventListener('mousemove', (e) => {
+  mousemove.style.left = e.pageX + 'px';
+  mousemove.style.top = e.pageY + 'px';
+});
+
+window.addEventListener('mousedown', () => {
+  mousemove.style.transform = 'scale(2) translate(-25%, -25%)';
+});
+
+window.addEventListener('mouseup', () => {
+  mousemove.style.transform = 'scale(1) translate(-50%, -50%)';
+  mousemove.style.border = '2px solid teal';
+});
+
+questionContainer.addEventListener('mouseenter', () => {
+  questionContainer.style.background = 'rgba(0,0,0,0.6)';
+});
+
+questionContainer.addEventListener('mouseout', () => {
+  questionContainer.style.background = 'pink';
+});
+
+response.addEventListener('mouseover', () => {
+  response.style.transform = 'rotate(2deg)';
+});
+
+//................................................................
+//KeyPress event
+
+const keypressContainer = document.querySelector('.keypress');
+const key = document.getElementById('key');
+
+// const ring = (key) => {
+//   const audio = new Audio();
+//   audio.src = `${key}.mp3`;
+//   audio.play();
+// };
+
+document.addEventListener('keypress', (e) => {
+  key.textContent = e.key;
+
+  if (e.key === 'j') {
+    keypressContainer.style.background = 'pink';
+  } else if (e.key === 'h') {
+    keypressContainer.style.background = 'teal';
+  } else {
+    keypressContainer.style.background = 'red';
+  }
+
+  // ring('e.key');
+});
+
+//--------------------------------------------------------
+// Scroll Event
+
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 120) {
+    nav.style.top = 0;
+  } else {
+    nav.style.top = '-50px';
+  }
+});
+
+//--------------------------------------------------------
+// Les Formulaires
+const inputName = document.querySelector('input[type="text"]');
+const select = document.querySelector('select');
+const form = document.querySelector('form');
+let pseudo = '';
+let language = '';
+
+console.log(select);
+
+inputName.addEventListener('input', (e) => {
+  pseudo = e.target.value;
+});
+
+select.addEventListener('input', (e) => {
+  language = e.target.value;
+});
+
+form.addEventListener('submit', () => {
+  console.log('yes !');
+});
