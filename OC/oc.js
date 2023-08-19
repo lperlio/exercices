f5c7fd745c0c4bee1e38714220d69a6c456984e7
lperlio <lionel.person@gmail.com>
2023-08-19 17:26:56 +0200

    Passion_Guitare_19_08_23

---
+++ OC/oc.js
@@ -0,0 +1,13 @@
+let totalLivres = 500;
+
+totalLivres += 50;
+totalLivres -= 10;
+totalLivres += 5;
+
+console.log(totalLivres);
+
+let affichageTotalLivres = 'Notre biblioèque possède ';
+affichageTotalLivres += totalLivres;
+affichageTotalLivres += ' livres.';
+
+console.log(affichageTotalLivres);
