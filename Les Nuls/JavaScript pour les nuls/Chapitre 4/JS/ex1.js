9a49bf51a2a266094fdc6adce15003aedf08449d
lperlio <lionel.person@gmail.com>
2023-11-10 11:17:48 +0100

    Les Nuls C3

---
+++ Les Nuls/JavaScript pour les nuls/Chapitre 4/JS/ex1.js
@@ -0,0 +1,23 @@
+let nomsChats = new Array('Minou', 'Ronron', 'Toto');
+console.log(nomsChats);
+
+let nomsChiens = ['Happy', 'Medor', 'Milou'];
+console.log(nomsChiens);
+
+let myJazz = [];
+myJazz[0] = 'Duke Ellington';
+myJazz[1] = 'Charlie Parker';
+myJazz[2] = 'Miles Davis';
+myJazz[99] = 'John Coltrane';
+console.log(myJazz.length);
+
+let meilleursAlbumsParGenre = [];
+meilleursAlbumsParGenre[0] = 'Country';
+meilleursAlbumsParGenre[0][0] = 'Johnnny Cash : Live';
+meilleursAlbumsParGenre[0][1] = 'patsy Cline';
+meilleursAlbumsParGenre[1] = 'Rock';
+console.log(meilleursAlbumsParGenre[0][1]);
+
+let monTableau = [];
+monTableau[2000];
+console.log(monTableau.length);
