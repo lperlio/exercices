9e0fedd804fa11be9714806e9c381b48b2122b2f
lperlio <lionel.person@gmail.com>
2023-12-19 12:03:17 +0100

    j2/2

--- decembre js/J2/index.js
+++ decembre js/J2/index.js
@@ -37,3 +37,75 @@ window.addEventListener('mousemove', (e) => {
   mousemove.style.left = e.pageX + 'px';
   mousemove.style.top = e.pageY + 'px';
 });
+
+window.addEventListener('mousedown', () => {
+  mousemove.style.transform = 'scale(2) translate(-25%, -25%)';
+});
+
+window.addEventListener('mouseup', () => {
+  mousemove.style.transform = 'scale(1) translate(-50%, -50%)';
+  mousemove.style.border = '2px solid teal';
+});
+
+questionContainer.addEventListener('mouseenter', () => {
+  questionContainer.style.background = 'rgba(0,0,0,0.6)';
+});
+
+questionContainer.addEventListener('mouseout', () => {
+  questionContainer.style.background = 'pink';
+});
+
+response.addEventListener('mouseover', () => {
+  response.style.transform = 'rotate(2deg)';
+});
+
+//---------------------------------------------------------------------------
+//KeyPress event
+
+const keypressContainer = document.querySelector('.keypress');
+const key = document.getElementById('key');
+const ring = (key) => {
+  const audio = new Audio();
+  audio.src = key + '.mp3';
+  audio.play();
+};
+
+document.addEventListener('keypress', (e) => {
+  key.textContent = e.key;
+
+  if (e.key === 'j') {
+    keypressContainer.style.background = 'pink';
+  } else if (e.key === 'h') {
+    keypressContainer.style.background = 'teal';
+  } else {
+    keypressContainer.style.background = 'red';
+  }
+
+  ring(e.key);
+});
+
+//------------------------------------------------------------------------------
+//Scroll Event
+
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+  // console.log(window.scrollY);
+
+  if (window.scrollY > 120) {
+    nav.style.top = 0;
+  } else {
+    nav.style.top = '-50px';
+  }
+});
+
+//-----------------------------------------------------------------------------
+//Formulaire Event
+
+const inputName = document.querySelector('input[type="text"]');
+const select = document.querySelector('select');
+let pseudo = '';
+
+inputName.addEventListener('input', (e) => {
+  pseudo = e.target.value;
+});
