5c636bf532e574337e3b00118bf1a2284d511e13
lperlio <lionel.person@gmail.com>
2023-12-26 16:19:35 +0100

    j7-2

---
+++ decembre js/JS-3-Cours/password-maker/index.js
@@ -0,0 +1,12 @@
+const dataLowercase = 'azertyuiopqsdfghjklmwxcvbn';
+const dataUppercase = dataLowercase.toUpperCase();
+const dataNumbers = '0123456789';
+const dataSymbols = '$*^ù!,!"(-é_ç)';
+
+console.log(dataUppercase);
+
+function generatePassword() {
+  console.log('Je génère un mot de passe');
+}
+
+generateButton.addEventListener('click', generatePassword);
