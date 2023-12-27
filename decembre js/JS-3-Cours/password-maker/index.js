0de3014a3438071c51ad0597de7b27ab1037e0dc
lperlio <lionel.person@gmail.com>
2023-12-27 09:28:37 +0100

    j8

--- decembre js/JS-3-Cours/password-maker/index.js
+++ decembre js/JS-3-Cours/password-maker/index.js
@@ -2,11 +2,41 @@ const dataLowercase = 'azertyuiopqsdfghjklmwxcvbn';
 const dataUppercase = dataLowercase.toUpperCase();
 const dataNumbers = '0123456789';
 const dataSymbols = '$*^ù!,!"(-é_ç)';
+const rangeValue = document.getElementById('password-length');
+const passwordOutput = document.getElementById('password-output');
 
-console.log(dataUppercase);
+console.log(rangeValue);
+
+// console.log(dataUppercase);
 
 function generatePassword() {
-  console.log('Je génère un mot de passe');
+  let data = [];
+  let password = '';
+
+  if (Lowercase.checked) data.push(...dataLowercase);
+  if (UpperCase.checked) data.push(...dataUppercase);
+  if (numbers.checked) data.push(...dataNumbers);
+  if (symbols.checked) data.push(...dataSymbols);
+
+  if (data.length === 0) {
+    alert('Veuillez sélectionner des critères');
+    return;
+  }
+
+  for (i = 0; i < rangeValue.ariaValueMax; i++) {
+    password += data[Math.floor(Math.random() * data.length)];
+    console.log(password);
+  }
+
+  passwordOutput.value = password;
+
+  passwordOutput.ariaSelected();
+  document.execCommand('copy');
+
+  generateButton.textContent = 'Copié !';
+
+  setTimeout(() => {}, 2000);
+  generateButton.textContent = 'Générer mot de passe';
 }
 
 generateButton.addEventListener('click', generatePassword);
